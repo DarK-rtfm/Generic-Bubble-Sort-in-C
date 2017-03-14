84c3a0e49881cd526f210613666db781ac8c5944
bidzina <b_aivazashvili@cu.edu.ge>
2017-03-14 12:44:44 +0400

    Bubble Sort

---
+++ main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Bubble Sort
+void sort(void * arr, size_t data_size, size_t elem_size, void (*compare)(void * x, void * y)){
+    size_t length = data_size/elem_size;
+    for(int i = 0; i<length; i++){
+        //printf("%d %d\n", arr + elem_size*i, arr + elem_size * (i+1));
+        for(int j = 0; j<length-1; j++){
+            compare(arr + elem_size*j, arr + elem_size * (j+1));
+        }
+
+    }
+}
+
+void compare_int(void * x, void * y){
+    int * value1 = (int*)x;
+    int * value2 = (int*)y;
+    int temp;
+    //ASC SORT
+    if(*value1 > *value2)  {
+        temp = *value1;
+        *value1 = *value2;
+        *value2 = temp;
+    }
+}
+
+void compare_char(void * x, void * y){
+    char * p1 = (char*)x;
+    char * p2 = (char*)y;
+    char ** value1 = (char**)x;
+    char ** value2 = (char**)y;
+    char temp;
+    //ASC SORT
+   if(**value1 > **value2){
+        temp = *p1;
+        *p1= *p2 ;
+        *p2= temp;
+   }
+
+}
+
+
+
+int main() {
+    int arr[] = {1, 7, 6, 5 , 22, 8, 1, 15, 74};
+    sort(arr, sizeof(arr), sizeof(int), compare_int);
+    for(int i =0; i<sizeof(arr)/sizeof(int); i++){
+        printf("%d ", arr[i]);
+    }
+
+
+    // char* c[] = {"3", "2", "1", "6", "5"};
+     char* c[] = {"c","j","b","a", "d", "f"};
+    sort(c, sizeof(c), sizeof(char*), compare_char);
+    for(int i = 0; i<sizeof(c)/sizeof(char*); i++){
+        printf("%c", *c[i]);
+    }
+
+}
