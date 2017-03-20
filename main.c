022d237dde08e7285fc62332e99d2b47ae17336d
bidzina <b_aivazashvili@cu.edu.ge>
2017-03-20 11:07:25 +0400

    swap in sort function

--- main.c
+++ main.c
@@ -1,43 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 
 //Bubble Sort
-void sort(void * arr, size_t data_size, size_t elem_size, void (*compare)(void * x, void * y)){
+void sort(void * arr, size_t data_size, size_t elem_size, int (*compare)(void * x, void * y)){
     size_t length = data_size/elem_size;
     for(int i = 0; i<length; i++){
         for(int j = 0; j<length-1; j++){
-            compare(arr + elem_size*j, arr + elem_size * (j+1));
+            if(compare(arr + elem_size*j, arr + elem_size * (j+1)) > 0){
+                char temp[elem_size];
+                memcpy(temp,arr + elem_size*j,elem_size);
+                memcpy(arr + elem_size*j,arr + elem_size * (j+1), elem_size);
+                memcpy(arr + elem_size * (j+1),temp, elem_size);
+            };
         }
 
     }
 }
 
-void compare_int(void * x, void * y){
+int compare_int(void * x, void * y){
     int * value1 = (int*)x;
     int * value2 = (int*)y;
-    int temp;
-    //ASC SORT
-    if(*value1 > *value2)  {
-        temp = *value1;
-        *value1 = *value2;
-        *value2 = temp;
-    }
+    return *value1 - *value2;
 }
 
-void compare_char(void * x, void * y){
+char compare_char(void * x, void * y){
     char * p1 = (char*)x;
     char * p2 = (char*)y;
-    char temp;
-   //ASC SORT
-   if(*p1 > *p2){
-        temp = *p1;
-        *p1= *p2 ;
-        *p2= temp;
-   }
-
+    return *p1 - *p2;
 }
-
 
 
 int main() {
@@ -48,7 +40,7 @@ int main() {
     }
 
 
-    // char c[] = {'3', '7', '5', '3'};
+//     char c[] = {'3', '7', '5', '3'};
     char c[] = {'c', 'd', 'b','f','g','a'};
     sort(c, sizeof(c), sizeof(char), compare_char);
     for(int i = 0; i<sizeof(c)/sizeof(char); i++){
