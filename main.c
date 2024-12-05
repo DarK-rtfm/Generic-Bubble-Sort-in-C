fe0a54d0383917db773a35849da59995f9d85cdc
dark-rtfm <44683230+DarK-rtfm@users.noreply.github.com>
2024-12-05 19:44:50 +0100

    ISO C Complience

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
 
 //Bubble Sort
 void sort(void * arr, size_t data_size, size_t elem_size, int(*compare)(void * x, void * y)){
+    char* arr_cptr = arr;
     size_t length = data_size/elem_size;
-    for(int i = 0; i<length; i++){
-        for(int j = 0; j<length-1; j++){
-            if(compare(arr + elem_size*j, arr + elem_size * (j+1)) > 0){
+    for(size_t i = 0; i<length; i++){
+        for(size_t j = 0; j<length-1; j++){
+            if(compare(arr_cptr + elem_size*j, arr_cptr + elem_size * (j+1)) > 0){
                 char temp[elem_size];
-                memcpy(temp,arr + elem_size*j,elem_size);
-                memcpy(arr + elem_size*j,arr + elem_size * (j+1), elem_size);
-                memcpy(arr + elem_size * (j+1),temp, elem_size);
+                memcpy(temp,arr_cptr + elem_size*j,elem_size);
+                memcpy(arr_cptr + elem_size*j,arr_cptr + elem_size * (j+1), elem_size);
+                memcpy(arr_cptr + elem_size * (j+1),temp, elem_size);
             };
         }
 
@@ -32,10 +32,10 @@ int compare_char(void * x, void * y){
 }
 
 
-int main() {
+int main(void) {
     int arr[] = {1, 7, 6, 5 , 22, 8, 1, 15, 74};
     sort(arr, sizeof(arr), sizeof(int), compare_int);
-    for(int i =0; i<sizeof(arr)/sizeof(int); i++){
+    for(size_t i =0; i<sizeof(arr)/sizeof(int); i++){
         printf("%d ", arr[i]);
     }
 
@@ -43,7 +43,7 @@ int main() {
 //     char c[] = {'3', '7', '5', '3'};
     char c[] = {'c', 'd', 'b','f','g','a'};
     sort(c, sizeof(c), sizeof(char), compare_char);
-    for(int i = 0; i<sizeof(c)/sizeof(char); i++){
+    for(size_t i = 0; i<sizeof(c)/sizeof(char); i++){
         printf("%c", c[i]);
     }
 
